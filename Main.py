103126cc1ccd93b1c8f284b3a1adf24054f9f281
ZacharyHelman2021 <828664@mystma.org>
2021-03-22 15:12:25 +0000

    Madlib

--- Main.py
+++ Main.py
@@ -1,1 +1,18 @@
-print("Zach's Favorite Movies:\nRaging Bull\nLord of the Rings\nOnce Upon a Time in the West\nSchindler's List\nCitizen Kane")
+
+adjective1 = input("Enter a adjective: ")
+adjective2 = input("Enter a adjective: ")
+bird = input("Enter a type of bird: ")
+room = input("Enter a room in the house: ")
+ptverb = input("Enter a past tense verb: ")
+verb = input("Enter a verb: ")
+relative = input("Enter a relative's name: ")
+noun = input("Enter a noun: ")
+liquid = input("Enter a liquid: ")
+verbing = input("Enter a verb ending with -ing: ")
+
+print("It was a " + adjective1 + " cold November day.")
+print("I woke up to the " + adjective2 + " smell of " + bird + " roasting in the " + room + " downstairs.")
+print("I " + ptverb + " down the stairs to see if I could help" + verb + " the dinner.")
+print("My mom said, 'See if " + relative + " needs a fresh" + noun + ".'")
+print("So I carried a tray of glasses full of " + liquid + " into the " + verbing + " room."
+)
